f7baf1f604b425adcafca22e90eacfccc42de040
Evan Bouman <evan.bouman@gmail.com>
2024-09-19 11:23:18 -0500

    Completed GET route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,15 @@
 console.log( 'js' );
 
 function getKoalas(){
-  console.log( 'in getKoalas' );
-  // axios call to server to get koalas
-  
-} // end getKoalas
+      axios({
+      method: 'GET',
+      url: '/koalas'
+    }).then(function(response) {
+      console.log('getKoalas() was called')
+    }).catch(function(error){
+      console.log('error in GET', error);
+    });
+  }  
 
 function saveKoala(){
   console.log( 'in saveKoala' );
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const koalaRouter = express.Router();
+const router = express.Router();
 
 const pool = require('../module/pool.js')
 
@@ -7,7 +7,17 @@ const pool = require('../module/pool.js')
 
 
 // GET
-
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM koalas ORDER BY "name";';
+    pool.query(queryText).then(result => {
+        // Sends back the results in an object
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error getting koalas', error);
+            res.sendStatus(500);
+        });
+});
 
 // POST
 
@@ -17,4 +27,4 @@ const pool = require('../module/pool.js')
 
 // DELETE
 
-module.exports = koalaRouter;
+module.exports = router;
